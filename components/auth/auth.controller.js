7ae11ecb1ba49f6e7adf690c77b211be09a04870
Edwin Risvianto <erisvianto@gmail.com>
2023-01-19 02:12:46 +0700

    add axios interceptors

--- components/auth/auth.controller.js
+++ components/auth/auth.controller.js
@@ -1,89 +1,101 @@
-const api = require("../../lib/axios");
-const client = require("../../lib/redis");
-const util = require("util");
+const api = require('../../lib/axios');
+const client = require('../../lib/redis');
+const util = require('util');
 const get = util.promisify(client.get).bind(client);
 
 exports.login = async (req, res) => {
-    try {
-        const { data } = await api.post(`/auth/login`, req.body);
+	try {
+		const { data } = await api.post(`/auth/login`, req.body);
 
-        const accessToken = data.access_token;
-        const tokenExp = data.expires_in;
-        client.set("access_token", accessToken);
-        client.expire("access_token", tokenExp);
+		const accessToken = data.access_token;
+		const tokenExp = data.expires_in;
+		client.set('access_token', accessToken);
+		client.expire('access_token', tokenExp);
 
-        const { access_token, token_type, expires_in, ...user } = data;
-        client.set("user", JSON.stringify(user));
+		const { role, ...user } = data.user;
+		user['role_id'] = data.user.role.id;
+		client.set('user', JSON.stringify(user));
 
-        const { role, chapter_finish, ...response } = data.user;
-        response["role_id"] = data.user.role.id;
+		const { permissions } = data.user?.role;
+		client.set('permissions', JSON.stringify(permissions));
 
-        res.status(200).send({
-            status: 200,
-            message: "Login successfully!",
-            data: response,
-        });
-    } catch (error) {
-        error?.response?.status
-            ? res.status(error?.response?.status).send(error.message)
-            : res.status(400).send(error.message);
-    }
+		res.status(200).send({
+			status: 200,
+			message: 'Login successfully!',
+			data: user,
+		});
+	} catch (error) {
+		error?.response?.status
+			? res.status(error?.response?.status).send(error.message)
+			: res.status(400).send(error.message);
+	}
 };
 
 exports.user = async (req, res) => {
-    try {
-        const token = await client.get("access_token");
-        const { data } = await api.get("/user", {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        });
+	try {
+		const { data } = await api.get('/user');
 
-        res.status(200).send({
-            status: 200,
-            message: "Get user successfully!",
-            data: data,
-        });
-    } catch (error) {
-        error?.response?.status
-            ? res.status(error?.response?.status).send({
-                  status: error.response.status,
-                  message: error.message,
-              })
-            : res.status(400).send({
-                  status: 400,
-                  message: error.message,
-              });
-    }
+		res.status(200).send({
+			status: 200,
+			message: 'Get user successfully!',
+			data: data,
+		});
+	} catch (error) {
+		error?.response?.status
+			? res.status(error?.response?.status).send({
+					status: error.response.status,
+					message: error.message,
+			  })
+			: res.status(400).send({
+					status: 400,
+					message: error.message,
+			  });
+	}
 };
 
 exports.logout = async (req, res) => {
-    const deleteToken = await client.del("access_token");
-    client.expire("access_token", 0);
-    if (deleteToken === 0) {
-        res.status(400).send({
-            status: 400,
-            message: "Logout failed! Token not found",
-        });
-        return;
-    }
+	const deleteToken = await client.del('access_token');
+	client.expire('access_token', 0);
+	if (deleteToken === 0) {
+		res.status(400).send({
+			status: 400,
+			message: 'Logout failed! Token not found',
+		});
+		return;
+	}
 
-    res.status(200).send({
-        status: 200,
-        message: "Logout successfully!",
-    });
+	res.status(200).send({
+		status: 200,
+		message: 'Logout successfully!',
+	});
 };
 
 exports.refresh = async (req, res) => {
-    try {
-        const token = await client.get("access_token");
-        const { data } = await api.post("/auth/refresh", {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        });
-        res.send(data)
-    } catch (error) {
-        res.send(error);
-    }
+	try {
+		const { data } = await api.post('/auth/refresh');
+
+		if (data.code === 401) {
+			res.status(401).send(data);
+			return;
+		}
+
+		const accessToken = data.access_token;
+		const tokenExp = data.expires_in;
+		client.set('access_token', accessToken);
+		client.expire('access_token', tokenExp);
+
+		const { role, ...user } = data.user;
+		user['role_id'] = data.user.role.id;
+		client.set('user', JSON.stringify(user));
+
+		res.send({
+			status: 200,
+			message: 'Refresh token sucessfully!',
+			data: user,
+		});
+	} catch (error) {
+		error?.response?.status
+			? res.status(error?.response?.status).send(error.message)
+			: res.status(400).send(error.message);
+	}
 };
--- components/homepage/homepage.controller.js
+++ components/homepage/homepage.controller.js
@@ -4,21 +4,15 @@ const client = require("../../lib/redis");
 exports.homepage = async (req, res) => {
     try {
         const userRes = await client.get("user");
-        const { user } = JSON.parse(userRes);
-        const token = await client.get("access_token");
-        const headers = {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        };
+        const user = JSON.parse(userRes);
 
-        const division = await api.get("/division/" + user.division_id, headers);
-        const latestNews = await api.get("/news/latest?approval_status=4", headers);
-        const allNews = await api.get("/news/all?approval_status=4&sort[created]=desc", headers);
-        const latestAnnouncement = await api.get("/announcement/latest?approval_status=4", headers);
-        const announcementPopUp = await api.get("/announcement/get-popup?approval_status=4", headers);
-        const allAnnouncement = await api.get("/announcement/all?approval_status=4&sort[created]=desc", headers);
-        const allEvent = await api.get("/event/all?approval_status=4&sort[created]=desc", headers);
+        const division = await api.get("/division/" + user.division_id);
+        const latestNews = await api.get("/news/latest?approval_status=4");
+        const allNews = await api.get("/news/all?approval_status=4&sort[created]=desc");
+        const latestAnnouncement = await api.get("/announcement/latest?approval_status=4");
+        const announcementPopUp = await api.get("/announcement/get-popup?approval_status=4");
+        const allAnnouncement = await api.get("/announcement/all?approval_status=4&sort[created]=desc");
+        const allEvent = await api.get("/event/all?approval_status=4&sort[created]=desc");
         
         const response = {
             "status" : 200,
