8ffb2b3018394e609dc9110ccd46a2d2f46d2302
EdwinR <edwin.risvianto@aqi.co.id>
2023-01-19 19:01:12 +0700

    change route logout to post

--- components/auth/auth.router.js
+++ components/auth/auth.router.js
@@ -4,7 +4,7 @@ const authController = require('./auth.controller');
 
 router.post('/login', authController.login);
 router.get('/user', authController.user);
-router.get('/logout', authController.logout);
+router.post('/logout', authController.logout);
 router.post('/refresh', authController.refresh);
 
 module.exports = router;
